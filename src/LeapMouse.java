71b54c7522241c89d8004c5f9bd969a09f54392d
Jose Eduardo <pepe.96.jose@gmail.com>
2014-12-01 00:41:33 -0500

    Add OnConnect and OnFrame methods

--- src/LeapMouse.java
+++ src/LeapMouse.java
@@ -1,9 +1,43 @@
+import com.leapmotion.leap.*;
+import java.awt.Dimension;
+import java.awt.Robot;
+
+class CustomListener extends Listener {
+	
+	public Robot robot;
+	
+	public void onConnect(Controller controller) {
+		controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+		controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+		controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+	}
+	
+	public void onFrame(Controller controller) {
+		try { 
+			robot = new Robot(); 
+		} catch (Exception e) {}
+		
+		Frame frame = controller.frame();
+		InteractionBox box = frame.interactionBox();
+		for (Finger f : frame.fingers()) {
+			if (f.type() == Finger.Type.TYPE_INDEX) {
+				Vector fingerPos = f.tipPosition();
+				Vector boxFingerPos = box.normalizePoint(fingerPos);
+				Dimension screen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+				robot.mouseMove((int) (screen.width * boxFingerPos.getX()), (int) (screen.height - boxFingerPos.getY() * screen.height));
+			}
+		}
+	}
+}
 
 public class LeapMouse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CustomListener listener = new CustomListener();
+		Controller controller = new Controller();
+		controller.addListener(listener);
+		
+		
 	}
 
 }
